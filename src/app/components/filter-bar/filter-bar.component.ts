f72fb3866b255017265d0029e8620b4b1fdc793a
Shoe2 <andrewelee1983@gmail.com>
2020-09-08 09:26:16 -0700

    filter and service

--- src/app/components/filter-bar/filter-bar.component.ts
+++ src/app/components/filter-bar/filter-bar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { BeerService } from 'src/app/services/beer.service';
+import { Filter } from 'src/app/classes/filter';
 
 @Component({
   selector: 'filter-bar',
@@ -21,13 +23,28 @@ export class FilterBarComponent implements OnInit {
   malt: string = null;
   food: string = null;
 
-  constructor() { }
+  constructor(private beerService: BeerService) { }
 
   ngOnInit(): void {
   }
 
   filterBeers(){
-    
+    const filter = new Filter;
+    filter.abv = this.abv;
+    filter.ibu = this.ibu;
+    filter.ebc = this.ebc;
+    filter.abvLessOrGreater = this.abvLessOrGreater;
+    filter.ibuLessOrGreater = this.ibuLessOrGreater;
+    filter.ebcLessOrGreater = this.ebcLessOrGreater;
+    filter.beer_name = this.beer_name;
+    filter.yeast = this.yeast;
+    filter.brewed_before = this.brewed_before;
+    filter.brewed_after = this.brewed_after; 
+    filter.hops = this.hops;
+    filter.malt = this.malt;
+    filter.food = this.food;
+
+    this.beerService.getBeers(filter);
   }
 
 }
--- src/app/services/beer.service.ts
+++ src/app/services/beer.service.ts
@@ -1,9 +1,41 @@
 import { Injectable } from '@angular/core';
+import { Filter } from '../classes/filter';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { BehaviorSubject, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { Beer } from '../classes/beer';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BeerService {
+  beersSource = new BehaviorSubject<Beer[]>([]);
+  beers = this.beersSource.asObservable();
 
-  constructor() { }
+  constructor(private $http: HttpClient) { }
+
+  getBeers(filter: Filter){
+    let url = "https://api.punkapi.com/v2/?brewed_before=" + filter.brewed_before + "&brewed_after=" + filter.brewed_after;
+
+    filter.abv != null && filter.abvLessOrGreater === "greaterThan" ? url += ("&abv_gt=" + filter.abv) : [/*do nothing */];
+    filter.abv != null && filter.abvLessOrGreater === "lessThan" ? url += ("&abv_lt=" + filter.abv) : [/*do nothing */];
+    filter.ibu != null && filter.ibuLessOrGreater === "greaterThan" ? url += ("&ibu_gt=" + filter.ibu) : [/*do nothing */];
+    filter.ibu != null && filter.ibuLessOrGreater === "lessThan" ? url += ("&ibu_lt=" + filter.ibu) : [/*do nothing */];
+    filter.ebc != null && filter.ebcLessOrGreater === "greaterThan" ? url += ("&ebc_gt=" + filter.ebc) : [/*do nothing */];
+    filter.ebc != null && filter.ebcLessOrGreater === "lessThan" ? url += ("&ebc_lt=" + filter.ebc) : [/*do nothing */];
+    filter.beer_name != null ? url += ("&beer_name=" + filter.beer_name) : [/*do nothing */];
+    filter.yeast != null ? url += ("&yeast=" + filter.yeast) : [/*do nothing */];
+    filter.hops != null ? url += ("&hops=" + filter.hops) : [/*do nothing */];
+    filter.malt != null ? url += ("&malt=" + filter.malt) : [/*do nothing */];
+    filter.food != null ? url += ("&food=" + filter.food) : [/*do nothing */];
+
+    this.$http.get(url)
+    .pipe(catchError((error: HttpErrorResponse)=>{
+      console.log(error);
+      return throwError(error);
+    }))
+    .subscribe((response: Beer[])=>{
+        this.beersSource.next(response);
+    });
+  }
 }
